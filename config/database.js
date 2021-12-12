2e21f8ed2bdd4fdbd07b0a760d612160a6ef2760
butterchoco <supri.contact@gmail.com>
2021-12-12 15:29:14 +0700

    refactor database config

--- config/database.js
+++ config/database.js
@@ -3,24 +3,40 @@ const { parse } = require("pg-connection-string");
 module.exports = ({ env }) => {
   const { host, port, database, user, password } = parse(env("DATABASE_URL"));
 
+  const pgDefaultHerokuSettings = {
+    connector: "bookshelf",
+    settings: {
+      client: "postgres",
+      host,
+      port,
+      database,
+      user,
+      password,
+      ssl: { rejectUnauthorized: false },
+    },
+    options: {
+      ssl: false,
+    },
+  };
+
   return {
     defaultConnection: "default",
     connections: {
-      default: {
-        connector: "bookshelf",
-        settings: {
-          client: "postgres",
-          host,
-          port,
-          database,
-          username: user,
-          password,
-          ssl: { rejectUnauthorized: false },
-        },
-        options: {
-          ssl: false,
-        },
-      },
+      default:
+        process.env.NODE_ENV === "development"
+          ? pgDefaultHerokuSettings
+          : {
+              connector: "bookshelf",
+              settings: {
+                client: "postgres",
+                host: process.env.DATABASE_HOST,
+                port: process.env.DATABASE_PORT,
+                database: process.env.DATABASE_NAME,
+                user: process.env.DATABASE_USERNAME,
+                password: process.env.DATABASE_PASSWORD,
+              },
+              options: {},
+            },
     },
   };
 };
